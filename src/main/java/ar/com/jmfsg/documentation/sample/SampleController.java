391a78ff23ec3e2d1ae2b18ad3c936f1bb050b06
Martin A. Miguel <mmiguel@despegar.com>
2013-08-29 16:25:14 -0300

    Small fixes

--- src/main/java/ar/com/jmfsg/documentation/sample/SampleController.java
+++ src/main/java/ar/com/jmfsg/documentation/sample/SampleController.java
@@ -1,8 +1,13 @@
 package ar.com.jmfsg.documentation.sample;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import ar.com.jmfsg.documentation.annotation.Documentation;
@@ -30,5 +35,17 @@ public class SampleController {
     	ModelAndView ret = createView();
         ret.addObject("dummy-key", "dummy-value");
         return ret;
+    }
+    
+    @RequestMapping(value = "/dynamic/{type}", method = {RequestMethod.GET, RequestMethod.POST})
+    public ModelAndView dynamic(@PathVariable(value="type") String type, 
+    		@RequestParam(value = "page", required = false) String page, 
+    		@RequestParam(value = "pagesize", required = false) String pageSize, @RequestBody(required = false) Map<String, String> body) {
+    	ModelAndView ret = createView();
+    	ret.addObject("type", type);
+    	ret.addObject("page", page);
+    	ret.addObject("pagesize", pageSize);
+    	ret.addObject("body", body);
+    	return ret;
     }
 }
